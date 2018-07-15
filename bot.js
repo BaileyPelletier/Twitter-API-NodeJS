394a0024f904753ba287fbd07ea21386df3a1130
Bailey Pelletier <baileypelletier@yahoo.com>
2018-07-14 20:31:03 -0400

    End of 15.4: Get/Post Calls
    Created get command to search tweets and post command to send out tweets

--- bot.js
+++ bot.js
@@ -5,3 +5,33 @@ var Twit = require('twit');
 var config = require('./config');
 // console.log(config)
 var T = new Twit(config)
+
+// var params = {
+//   q: 'fire',
+//   count: 5
+// };
+//
+// T.get('search/tweets', params, gotData);
+//
+// function gotData (err, data, response) {
+//   var tweets = data.statuses;
+//   for (var i = 0; i < tweets.length; i++) {
+//     console.log(tweets[i].text);
+//     console.log(' ');
+//   }
+//   // console.log(data);
+// };
+
+
+var tweet =  {
+  status: '#SkynetBooting'
+}
+
+T.post('statuses/update', tweet , tweeted);
+
+function tweeted(err, data, response) {
+  if (err) {
+    console.log("Something went wrong!")
+  }
+  console.log("It worked!")
+}
