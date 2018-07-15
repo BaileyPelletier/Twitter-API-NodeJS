6d42a6729bb6e5c79054acdb9d7c09edf6f6dbc3
Bailey Pelletier <baileypelletier@yahoo.com>
2018-07-14 22:01:57 -0400

    15.5: Can now react to events

--- bot.js
+++ bot.js
@@ -6,6 +6,8 @@ var config = require('./config');
 // console.log(config)
 var T = new Twit(config)
 
+
+// SEARCHES TWEETS WITH T.get
 // var params = {
 //   q: 'fire',
 //   count: 5
@@ -22,16 +24,95 @@ var T = new Twit(config)
 //   // console.log(data);
 // };
 
+function todayStr () {
+  var today = new Date();
+  var day = today.getDate();
+  var year = today.getFullYear();
+  var monthNum = today.getMonth();
+  var month = null;
+  switch (monthNum) {
+    case 0:
+      month = "January";
+      break;
+    case 1:
+      month = "February";
+      break;
+    case 2:
+      month = "March";
+      break;
+    case 3:
+      month = "April";
+      break;
+    case 4:
+      month = "May";
+      break;
+    case 5:
+      month = "June";
+      break;
+    case 6:
+      month = "July";
+      break;
+    case 7:
+      month = "August";
+      break;
+    case 8:
+      month = "September";
+      break;
+    case 9:
+      month = "October";
+      break;
+    case 10:
+      month = "November";
+      break;
+    case 11:
+      month = "December";
+      break;
+    default:
+      month = "(Not sure what month)";
+  }
+  return month + " " + day + ", " + year;
+}
+console.log(todayStr());
+//Setting up a user stream
+var stream = T.stream('user');
 
-var tweet =  {
-  status: '#SkynetBooting'
+//Anytime someone follows me
+stream.on('follow', followed)
+
+function followed(event) {
+  console.log("Follow Event!");
+  var name = event.source.name;
+  var screenName = event.source.screen_name;
+  tweetIt("@" + screenName + ", you are so rad for following me on " + todayStr() + "! Thanks!");
 }
 
-T.post('statuses/update', tweet , tweeted);
 
-function tweeted(err, data, response) {
-  if (err) {
-    console.log("Something went wrong!")
+
+
+
+// POSTS TWEETS
+// tweetIt();
+// setInterval(tweetIt, 1000*20);    //Sets an interval in which tweets (tweetIt) are executed
+
+function tweetIt(txt) {
+  var r = Math.floor(Math.random()*100);
+
+  if (txt) {
+    var tweet = {
+      status: txt
+    }
+  } else {
+    var tweet =  {
+      status: 'Here is a random number for you: ' + r + " You're welcome. #RandomNumberGeneration"
+    }
   }
-  console.log("It worked!")
+
+  T.post('statuses/update', tweet , tweeted);
+
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log("Something went wrong!")
+    }
+    console.log("It worked!")
+  }
 }
