7169d9f60493d81eb38d68f302e305fc685e46ad
Bailey Pelletier <baileypelletier@yahoo.com>
2018-10-23 20:45:50 -0400

    Update config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  consumer_key:         'k5m8yomsvU30cFbmvzrEZBgXj',
+  consumer_secret:      '69H7bIjQBT9HWP3Bs5vKoogs41ootvq0VHDIxZ5Njx8fYkIZcn',
+  access_token:         '1018241787918462977-6Wm15vJTpt23xDlFsauvUWHhAegpIS',
+  access_token_secret:  'X2utyIYUGqjxbzjDg2UOkqerySrVKvJKK93sZPj0TK8LA',
+}
